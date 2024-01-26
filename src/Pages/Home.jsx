dbcc79535b155c387821be1c202dd3938a4ecf89
Mohnish <mohnishkalaimani@gmail.com>
2024-01-26 11:47:20 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-blue'>
+    <div className='text-red-500'>
       Home Page
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [isMenuOpen,setIsMenuOpen] = useState(false);
-    const handleMenuToggler = () => [
-        setIsMenuOpen(!isMenuOpen)
-    ]
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggler = () => [setIsMenuOpen(!isMenuOpen)];
   return (
     <nav>
-      <a href='/'>WorkSpace</a>
+      <a href="/" className="flex items-center gap-2 text-2xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="29"
+          height="30"
+          viewBox="0 0 29 30"
+          fill="none"
+        >
+          <circle cx="12.0143" cy="12.5143" r="12.0143" fill="#3575E2" fillOpacity={0.4} />
+          <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#3575E2" />
+        </svg>
+        <span>WorkSpace</span>
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
